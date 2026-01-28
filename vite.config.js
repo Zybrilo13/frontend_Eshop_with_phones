b7c1803495a23d9d1a99b80229ee9c98de4a424d
Yevhenii <123zhenia1234@gmail.com>
2026-01-28 16:18:10 +0100

    second commit

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/frontend_Eshop_with_phones/',
   plugins: [
     vue(),
     vueDevTools(),
